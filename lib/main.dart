c132a02c66287aa2cd1534431c370a44b68d574c
EYoung75 <eyoung75.ey@gmail.com>
2019-11-07 13:09:21 -0600

    added places provider into main.dart to begin implementation of provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,21 @@
 import "package:flutter/material.dart";
+import "package:provider/provider.dart";
+import "./providers/placeProvider.dart";
+import "./screens/placesScreen.dart";
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Great Places",
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.amber
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: "Great Places",
+        theme:
+            ThemeData(primarySwatch: Colors.indigo, accentColor: Colors.amber),
+        home: PlacesList(),
       ),
-      home: Home(),
-    );
-  }
-}
-
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      
     );
   }
 }
